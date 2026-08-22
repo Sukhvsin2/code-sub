44484e9e1287f8f3b6d82dd52cdd48d25a3bfbc3
Sukhvinder Singh <46042060+Sukhvsin2@users.noreply.github.com>
2026-08-22 13:54:10 -0700

    Add: string-encode-and-decode - submission-6

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-6.go
@@ -0,0 +1,28 @@
+type Solution struct{}
+
+func (s *Solution) Encode(strs []string) string {
+	var encoded_str string
+	for _, s := range strs{
+		encoded_str += strconv.Itoa(len(s)) + "#" + s
+	}
+
+	return encoded_str
+}
+
+func (s *Solution) Decode(encoded string) []string {
+	fmt.Println(encoded)
+	words := []string{}
+	i:=0
+	for i < len(encoded){
+		j := i
+		for string(encoded[j]) != "#"{
+			j += 1
+		}
+		// now b/w i and j we have the word's length number
+		length, _ := strconv.Atoi(encoded[i:j])
+		words = append(words, encoded[j+1 : j+1+length])
+		i = j+1+length
+	}
+
+	return words
+}
