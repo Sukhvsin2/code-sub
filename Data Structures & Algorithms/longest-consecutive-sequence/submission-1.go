a52e7420938106e8963706789900994d749942d7
Sukhvinder Singh <46042060+Sukhvsin2@users.noreply.github.com>
2026-08-29 16:47:23 -0700

    Add: longest-consecutive-sequence - submission-1

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-1.go
@@ -0,0 +1,31 @@
+func longestConsecutive(nums []int) int {
+	set := make(map[int]struct{})
+
+	for _, n := range nums{
+		set[n] = struct{}{}
+	}
+
+	longest := 0
+	fmt.Println(set)
+	for n, _ := range set{
+		_, exists := set[n-1]
+		if !exists {
+			length := 0
+
+			run := true
+			for run{
+				if _, increment_exists := set[n+length]; !increment_exists{
+					run = false
+					break
+				}
+				length++
+
+			}
+
+			longest = max(longest, length)
+		}
+
+	}
+
+	return longest
+}
