4fd6c9386d9e94154b08e44aa7c7cb8793c9a18a
Sukhvinder Singh <46042060+Sukhvsin2@users.noreply.github.com>
2026-08-29 17:03:54 -0700

    Add: longest-consecutive-sequence - submission-5

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-5.go
@@ -0,0 +1,22 @@
+import "slices"
+
+func longestConsecutive(nums []int) int {
+	if len(nums) < 1{
+		return 0
+	}
+	// sort() and set()
+	slices.Sort(nums)
+	nums = slices.Compact(nums)
+
+	count := 0
+	length := 0
+	for i:=0; i<len(nums)-1; i++{
+		if nums[i]+1 != nums[i+1]{
+			length = 0
+			continue
+		}
+		length++
+		count = max(count, length)
+	}
+	return count+1
+}
