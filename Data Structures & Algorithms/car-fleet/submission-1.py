c11d720baa8c95209c0a0ab92231b9f4ed1e4c63
Sukhvinder Singh <46042060+Sukhvsin2@users.noreply.github.com>
2026-09-13 23:33:16 -0700

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        
+        stack = []
+        # pairs = []
+        # # create pairs
+        # for i in range(len(position)):
+        #     pairs.append((position[i], speed[i]))
+        
+        pairs = [[p, s] for p, s in zip(position, speed)]
+
+        # sorted pairs
+        pairs = sorted(pairs)[::-1]
+
+        for p, s in pairs:
+            # cal time = (distance - covered dis ) / speed
+            stack.append((target-p)/s)
+            # need at least 2 car's time to calculate the fleet. 
+            # if recent added car's time is less or equal it means they will match speed.
+            # which means this the current car either surpass or match the speed.
+            # so count in single fleet so pop the recent added car's time.
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+
+
+        return len(stack)
